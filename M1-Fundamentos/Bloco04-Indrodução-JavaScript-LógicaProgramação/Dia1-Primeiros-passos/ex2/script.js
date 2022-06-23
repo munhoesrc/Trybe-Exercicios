1b82a8ea9065cec56679669d97addb69d9c61b5a
Renan Casarin Munhoes <munhoesrc@gmail.com>
2022-06-23 01:29:08 -0300

    exercicio 2

---
+++ M1-Fundamentos/Bloco04-Indrodução-JavaScript-LógicaProgramação/Dia1-Primeiros-passos/ex2/script.js
@@ -0,0 +1,19 @@
+let patientId = 50;
+let isEnrolled = true;
+
+const patientInfo = {
+    firstName: "Ana",
+    lastName: "Santos",
+};
+
+const patientEmail = "ana@email.com";
+
+console.log(typeof patientId, isEnrolled, patientInfo, patientEmail);
+
+const base = 5;
+let height = 8;
+const area = base * height;
+console.log(area);
+
+const perimeter = base + base + height + height;
+console.log(perimeter);
