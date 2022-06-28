5f9650be3754e46dc30ae5db24eb4ecde4377897
Renan Casarin Munhoes <munhoesrc@gmail.com>
2022-06-28 18:14:07 -0300

    conteudo do dia e ex1

---
+++ M1-Fundamentos/Bloco5-JavaScript-DOM-Eventos-WebStorage/Dia-1-DOM-e-Seletores/Conteúdo/script.js
@@ -0,0 +1,15 @@
+document.querySelector(".paragraph").style.color = "red";
+
+document.querySelector("#page-title").innerText = "X-man evolution";
+
+document.querySelectorAll("p")[0].innerText = "Segundo parágrago mudado com sucesso";
+document.querySelectorAll("p")[1].style.backgroundColor = "green";
+
+document.getElementById("subtitle").innerText = "Subtítulo com alteração!";
+document.getElementById("subtitle").style.color = "blue";
+
+// document.getElementsByClassName("paragraph")[0].innerText = "mundaça!";
+// document.getElementsByClassName("paragraph")[0].style.color = "red";
+// document.getElementsByClassName("paragraph")[0].style.backgroundColor = "green";
+
+// document.getElementsByTagName("h4")[0].style.color = "blue";
---
+++ M1-Fundamentos/Bloco5-JavaScript-DOM-Eventos-WebStorage/Dia-1-DOM-e-Seletores/Exercícios/Ex1/script.js
@@ -0,0 +1,14 @@
+document.getElementById("footer-container").style.backgroundColor = "#003533";
+
+document.getElementById("header-container").style.backgroundColor = "#00B069";
+
+document.querySelector(".emergency-tasks").style.backgroundColor = "#FF9F84";
+
+document.querySelector(".no-emergency-tasks").style.backgroundColor = "#F9DB5E";
+
+document.querySelectorAll("h3")[0].style.backgroundColor = "#A500F3";
+document.querySelectorAll("h3")[1].style.backgroundColor = "#A500F3";
+
+document.querySelectorAll("h3")[2].style.backgroundColor = "#232525";
+document.querySelectorAll("h3")[3].style.backgroundColor = "#232525";
+
