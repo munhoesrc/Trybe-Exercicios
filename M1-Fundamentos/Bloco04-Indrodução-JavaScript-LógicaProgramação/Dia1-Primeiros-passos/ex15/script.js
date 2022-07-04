8ce8020dc2805283ec583b14db06cef1f6d5a53a
Renan Casarin Munhoes <munhoesrc@gmail.com>
2022-07-04 13:53:19 -0300

    exercicios 15

---
+++ M1-Fundamentos/Bloco04-Indrodução-JavaScript-LógicaProgramação/Dia1-Primeiros-passos/ex15/script.js
@@ -0,0 +1,10 @@
+// Check if there is an odd number -----------------------------
+const num4 = 2;
+const num5 = 2;
+const num6 = 2;
+
+if (num4 % 2 !== 0 || num5 % 2 !== 0 || num6 % 2 !== 0) {
+  console.log(true);
+} else {
+  console.log(false);
+}
