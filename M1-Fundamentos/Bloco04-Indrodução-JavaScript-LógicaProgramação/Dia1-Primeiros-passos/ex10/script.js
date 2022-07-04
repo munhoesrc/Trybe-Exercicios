1e331756bc8107f6b70eb9a9ec3ce80ae2932cbc
Renan Casarin Munhoes <munhoesrc@gmail.com>
2022-07-04 13:47:33 -0300

    exercicios 10

---
+++ M1-Fundamentos/Bloco04-Indrodução-JavaScript-LógicaProgramação/Dia1-Primeiros-passos/ex10/script.js
@@ -0,0 +1,10 @@
+// Checks positive and negative number -------------------------------------
+const number = 0;
+
+if (number > 0) {
+  console.log("Number is positive.");
+} else if (number < 0) {
+  console.log("Number is negative.");
+} else {
+  console.log("Number is 0.");
+}
