c07275659e78f5e25387d70f28f7e15b56a4c133
Renan Casarin Munhoes <munhoesrc@gmail.com>
2022-07-04 13:52:25 -0300

    exercicios 14

---
+++ M1-Fundamentos/Bloco04-Indrodução-JavaScript-LógicaProgramação/Dia1-Primeiros-passos/ex14/script.js
@@ -0,0 +1,10 @@
+// Check if there is an even number -----------------------------
+const num1 = 1;
+const num2 = 1;
+const num3 = 3;
+
+if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0) {
+  console.log(true);
+} else {
+  console.log(false);
+}
