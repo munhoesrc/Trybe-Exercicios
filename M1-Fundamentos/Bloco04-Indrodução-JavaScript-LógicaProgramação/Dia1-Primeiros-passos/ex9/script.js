c368325043c404fd4433fb62f72a4c1496714234
Renan Casarin Munhoes <munhoesrc@gmail.com>
2022-07-04 13:44:03 -0300

    exercicios 9

---
+++ M1-Fundamentos/Bloco04-Indrodução-JavaScript-LógicaProgramação/Dia1-Primeiros-passos/ex9/script.js
@@ -0,0 +1,12 @@
+// Returns higher number -------------------------------------
+const e = 5;
+const f = 10;
+const g = 15;
+
+if (e > f && e > g) {
+  console.log(e);
+} else if (f > e && f > g) {
+  console.log(f);
+} else {
+  console.log(g);
+}
