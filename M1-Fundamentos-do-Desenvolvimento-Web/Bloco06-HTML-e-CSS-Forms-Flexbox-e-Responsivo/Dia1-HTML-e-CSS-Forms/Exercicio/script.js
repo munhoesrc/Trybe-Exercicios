b016d87d51d85d421ff0426440a13fba8363d5f7
Renan Casarin Munhoes <munhoesrc@gmail.com>
2022-07-08 13:56:28 -0300

    exercicio trybe trip - dia 6.1 ok

--- M1-Fundamentos-do-Desenvolvimento-Web/Bloco06-HTML-e-CSS-Forms-Flexbox-e-Responsivo/Dia1-HTML-e-CSS-Forms/Exercicio/script.js
+++ M1-Fundamentos-do-Desenvolvimento-Web/Bloco06-HTML-e-CSS-Forms-Flexbox-e-Responsivo/Dia1-HTML-e-CSS-Forms/Exercicio/script.js
@@ -1,4 +1,24 @@
 const submitButton = document.querySelector("#submit-button");
-  submitButton.onclick = (e) => {
-    // e.preventDefault();
-  };
+submitButton.onclick = (e) => {
+  // e.preventDefault();
+  validationButton();
+};
+function validationButton() {
+  
+  // Validar nome
+  const fullNameInput = document.querySelector("#full-name");
+  if (fullNameInput.value.length < 10 || fullNameInput.value.length > 40) {
+    alert("Dados inválidos.");
+    return;
+  }
+
+  // Validar email
+  const emailInput = document.querySelector("#email");
+  if (emailInput.value.length < 10 || emailInput.value.length > 50) {
+    alert("Dados inválidos.");
+    return;
+  }
+  alert(
+    "Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip."
+  );
+}
