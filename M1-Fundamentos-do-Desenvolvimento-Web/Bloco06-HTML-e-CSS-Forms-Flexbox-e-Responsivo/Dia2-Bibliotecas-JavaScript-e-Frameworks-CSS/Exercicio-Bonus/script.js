2b4789bd398a8ecc9a20e3ed8efc3c2fd627da9a
Renan Casarin Munhoes <munhoesrc@gmail.com>
2022-07-11 12:48:14 -0300

    exercicio bonus uso de biblioteca JavaScript - Pikaday

--- M1-Fundamentos-do-Desenvolvimento-Web/Bloco06-HTML-e-CSS-Forms-Flexbox-e-Responsivo/Dia2-Bibliotecas-JavaScript-e-Frameworks-CSS/Exercicio-Bonus/script.js
+++ M1-Fundamentos-do-Desenvolvimento-Web/Bloco06-HTML-e-CSS-Forms-Flexbox-e-Responsivo/Dia2-Bibliotecas-JavaScript-e-Frameworks-CSS/Exercicio-Bonus/script.js
@@ -22,3 +22,32 @@ function validationButton() {
     "Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip."
   );
 }
+
+/* var picker = new Pikaday({
+  field: document.getElementById('datepicker'),
+  format: 'D MMM YYYY',
+  onSelect: function() {
+      console.log(this.getMoment().format('Do MMMM YYYY'));
+  }
+}); */
+
+var picker = new Pikaday({
+  field: document.getElementById('datepicker'),
+  format: 'D/M/YYYY',
+  toString(date, format) {
+      // you should do formatting based on the passed format,
+      // but we will just return 'D/M/YYYY' for simplicity
+      const day = date.getDate();
+      const month = date.getMonth() + 1;
+      const year = date.getFullYear();
+      return `${day}/${month}/${year}`;
+  },
+  parse(dateString, format) {
+      // dateString is the result of `toString` method
+      const parts = dateString.split('/');
+      const day = parseInt(parts[0], 10);
+      const month = parseInt(parts[1], 10) - 1;
+      const year = parseInt(parts[2], 10);
+      return new Date(year, month, day);
+  }
+});
