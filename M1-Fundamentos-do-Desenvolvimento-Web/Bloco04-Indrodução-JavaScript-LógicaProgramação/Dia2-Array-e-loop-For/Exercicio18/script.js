6ceca22cda2e4c06b4a8c8032576e6491e8298f8
Renan Casarin Munhoes <munhoesrc@gmail.com>
2022-07-11 22:42:42 -0300

    M1-Bloco04-Exercicio-18

--- M1-Fundamentos-do-Desenvolvimento-Web/Bloco04-Indrodução-JavaScript-LógicaProgramação/Dia2-Array-e-loop-For/Exercicio18/script.js
+++ M1-Fundamentos-do-Desenvolvimento-Web/Bloco04-Indrodução-JavaScript-LógicaProgramação/Dia2-Array-e-loop-For/Exercicio18/script.js
@@ -1,4 +1,10 @@
-let menu = ["Home", "Serviços", "Portfólio", "Links"];
-let indexOfPortfolio = menu.indexOf("Portfólio");
+const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-console.log(indexOfPortfolio);
+// Multiplying numbers
+let multipliedNumbers = [];
+
+for (let firstIndex = 0; firstIndex < numbers.length - 1; firstIndex += 1) {
+  multipliedNumbers.push(numbers[firstIndex] * numbers[firstIndex + 1]);
+}
+
+console.log(multipliedNumbers);
