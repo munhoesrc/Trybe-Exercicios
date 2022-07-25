4e9f6d6bbd94dbdb9e674a2e8f4bf8553261d5d1
Renan Casarin Munhoes <munhoesrc@gmail.com>
2022-07-25 15:59:43 -0300

    conteudo dia 25/07

---
+++ M1-Fundamentos-do-Desenvolvimento-Web/Bloco08-Higher-Order-Functions-do-JavaScript-ES6/Dia3-map-e-filter/map01.js
@@ -0,0 +1,27 @@
+//mesma função utilizando for e em seguinda o map:
+
+// usando for:
+/* const persons = [
+  { firstName: 'Maria', lastName: 'Ferreira' },
+  { firstName: 'João', lastName: 'Silva' },
+  { firstName: 'Antonio', lastName: 'Cabral' },
+];
+
+const fullNames = [];
+
+for (let index = 0; index < persons.length; index += 1) {
+  fullNames.push(`${persons[index].firstName} ${persons[index].lastName}`);
+}
+
+console.log(fullNames); */
+
+// usando map:
+const persons = [
+  { firstName: 'Maria', lastName: 'Ferreira' },
+  { firstName: 'João', lastName: 'Silva' },
+  { firstName: 'Antonio', lastName: 'Cabral' },
+];
+
+const fullNames = persons.map((person) => `${person.firstName} ${person.lastName}`);
+
+console.log(fullNames); // [ 'Maria Ferreira', 'João Silva', 'Antonio Cabral' ]
