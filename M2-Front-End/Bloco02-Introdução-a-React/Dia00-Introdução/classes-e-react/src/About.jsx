7cf5ef7be0060edd4ad0177e4b17d665f5fb6fdf
Renan Casarin Munhoes <munhoesrc@gmail.com>
2022-08-16 12:10:52 -0300

    bloco 10 - exercicio done

---
+++ M2-Front-End/Bloco02-Introdução-a-React/Dia00-Introdução/classes-e-react/src/About.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class About extends React.Component {
+  render() {
+  const skills = ['HTML', 'CSS', 'JAVASCRIPT', 'JEST']
+  const jsxSkills = skills.map((skill) => <li>{skill}</li>)
+
+    return (
+    <div className="About">
+    <h1>Renan Casarin Munhoes</h1>
+    <p>
+      Olá meu nome é Renan e sou apaixonado por tecnologia, desenvolvimento e
+      marcas que trabalhem para contribuir com uma melhor interação digital.
+      Sou formado Bacharel em Administração no ano de 2015, e sempre trabalhei
+      na área, desempenhando várias atribuições. Atualmente passo por
+      transição de carreira, onde busco linkar a experiência adquirida com a
+      área Tech.
+    </p>
+    <h2>Minhas Habilidades</h2>
+    <ul>{ jsxSkills }</ul>
+  </div>
+  );
+  }
+}
+
+export default About;
